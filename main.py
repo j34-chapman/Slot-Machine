bce541c6d58e3e8fe70b4f35149af66874abe3b1
j34-chapman <jakechapman710@gmail.com>
2023-11-15 17:59:54 +0000

    Progressing

--- main.py
+++ main.py
@@ -1,8 +1,61 @@
-
+import random
 # Global constant never changes declared at the top with capitals
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3 
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+# Function to simulate a slot machine spin
+def get_slot_machine_spin(rows, cols, symbols):
+    # Initialize an empty list to store all symbols based on their counts
+    all_symbols = []
+
+    #  Iterate over each symbol and its count in the symbols dictionary 
+    # Im creating the key and value on the spot and appending to symbols dictionary
+    #    Key         Value    in   dictionary.symbols
+    for symbol_key, symbol_value in symbols.items():
+        # Repeat the appending of the symbol to all_symbols according to its count
+        for _ in range(symbol_value):
+            all_symbols.append(symbol_key)
+
+    # Initialize a list to store columns in the slot machine
+    columns = []
+
+    # Iterate over the specified number of columns
+    for _ in range(cols):
+        # Initialize an empty list to represent a column
+        column = []
+
+        # Create a copy of all_symbols to work with
+        current_symbols = all_symbols[:]
+
+        # Iterate over the specified number of rows
+        for _ in range(rows):
+            # Choose a random symbol from the current_symbols list
+            value = random.choice(current_symbols)
+            
+            # Remove the chosen symbol from the current_symbols list to avoid repetition
+            current_symbols.remove(value)
+            
+            # Append the chosen symbol to the column
+            column.append(value)
+
+        # Append the completed column to the columns list
+        columns.append(column)
+
+    # Return the list of columns representing the slot machine spin
+    return columns
+
+
 
 def deposit():
     while True :
@@ -50,7 +103,7 @@ def get_bet():
             if MIN_BET <= amount <= MAX_BET:
                 break
             else:
-                print (f"Amount must be between £{MIN_BET} - £{MAX_BET}.")
+                print (f"Amount must be between £{MIN_BET} - £{MAX_BET}.") # Another way but cleaner than ln 29
         else:
             print("Enter a number")
 
@@ -60,7 +113,21 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
+    
+    while True:
+        bet = get_bet()
+        total_bet =bet * lines 
+        
+        if total_bet > balance:
+            print(f"You do not have enough money to bet that amount . Your current balance : £{balance}")
+            
+        else:
+            break
+        
+    
+  
+    print(f"You are betting on £{bet} on {lines} lines. Total bet is equal to: £{total_bet}")
     print(balance,lines)
+    
 
 main()
